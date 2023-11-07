ef68b1f75a0c1cbbc9c1bbad911fbc9b9c8e324d
Didier BOISSINOT <didier.boissinot@sii.fr>
2023-11-07 14:13:47 +0100

    import firebase2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 // eslint-disable-next-line no-unused-vars
-//import firebase from "./FirebaseConfig"
+import firebase from "./FirebaseConfig"
+//import firebase from 'firebase/compat/app';
+
+import 'firebase/compat/firestore';
 
 function App() {
   return (
     <div className="App">
       <div className="title-row">
-        <h1 className="title">Firebase Recipes</h1>
+        <h1 className="title">Firebase Recipes Julien</h1>
       </div>
     </div>
   );
