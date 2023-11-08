45add092cea42d8e5098cd8e815e07e059e7867c
Didier BOISSINOT <didier.boissinot@sii.fr>
2023-11-08 10:46:57 +0100

    test julien 2

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <div className="title-row">
-        <h1 className="title">Firebase Recipes Julien</h1>
+        <h1 className="title">Firebase Recipes Toto</h1>
       </div>
     </div>
   );
